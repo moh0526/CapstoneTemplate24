4c60ff98cd9f5a803c59b683a24d874412ef81d4
moh0526 <133300375+moh0526@users.noreply.github.com>
2024-05-01 19:53:13 -0700

    Add files via upload

--- CapstoneTemplate24/app/routes/default.py
+++ CapstoneTemplate24/app/routes/default.py
@@ -4,8 +4,8 @@ from flask import render_template
 # This is for rendering the home page
 @app.route('/') #ask why this isnt working
 def index():
-    return render_template('index.html')
+    return render_template('teachers.html')
 
-@app.route('/aboutme')
-def aboutme():
-    return render_template('aboutme.html')
+# @app.route('/aboutme')
+# def aboutme():
+#     return render_template('aboutme.html')
